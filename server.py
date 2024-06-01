13f6a244257d1439778d8069fe36effc62eaad50
tangshitao <tst17@my.swjtu.edu.cn>
2024-06-01 21:50:36 +0800

    Feat: When a new ssh connection arrives, message history are available for him

--- server.py
+++ server.py
@@ -3,14 +3,24 @@ import websockets
 import json
 
 connected_clients = set()
+message_history = []
 
 async def handler(websocket, path):
     # 将新连接的客户端添加到集合中
     connected_clients.add(websocket)
+    
+    # 发送历史消息给新连接的客户端
+    for message in message_history:
+        await websocket.send(message)
+    
     try:
         async for message in websocket:
             data = json.loads(message)
             if data['type'] == 'message':
+                # 将消息添加到历史消息列表
+                message_history.append(json.dumps(data))
+                
+                # 广播消息给所有连接的客户端
                 broadcast_message = json.dumps(data)
                 await asyncio.wait([client.send(broadcast_message) for client in connected_clients])
     finally:
